04947e74fbb74e59fb61949e9beb5abbc351d3ef
nangonglinger <1307998991@qq.com>
2022-12-01 18:48:13 +0800

    操作符

---
+++ 位操作符/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+//a = 3,b = 5,把ab交换，但是不能使用第三个变量
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("%d %d", a, b);
+//	return 0;
+//}
