e96a0499bb351be0d6e07f86636776bffdd50294
nangonglinger <1307998991@qq.com>
2022-12-03 16:11:22 +0800

    操作符3

--- 位操作符/test.c
+++ 位操作符/test.c
@@ -13,10 +13,23 @@
 //	return 0;
 //}
 
-int main()
-{
-	int a = 1;
-	a = a << 2;
-	printf("%d", a);
-	return 0;
-}
+//int main()
+//{
+//	short a = 1;
+//	a = a << 2;
+//	printf("%d\n", a);
+//	printf("%d\n", sizeof(a));
+//	return 0;
+//}
+//
+//
+//int main()
+//{
+//	int a = -1;
+//	int b = ~a;
+//	char c = '0';
+//	printf("%d\n", a);
+//	printf("%d\n", b);
+//	printf("%d\n", sizeof(c));
+//	return 0;
+//}
