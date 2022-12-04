443fbd5354bf5d701a5790e0509528d6f17ae51b
nangonglinger <1307998991@qq.com>
2022-12-04 09:32:25 +0800

    操作符作业完成

--- 操作符/作业.c
+++ 操作符/作业.c
@@ -138,6 +138,16 @@ int main()
 {
 	int m = 1999;
 	int n = 2299;
-
+	int i = 0;
+	int count = 0;
+	for (i = 0; i < sizeof(m) * 8; i++)
+	{
+		int j = 1 << i;
+		if ((((~j) & m) == m) != (((~j) & n) == n))
+		{
+			count++;
+		}
+	}
+	printf("%d", count);
 	return 0;
 }
