07385b0ef81d54a71003275b8e40000232726261
nangonglinger <1307998991@qq.com>
2022-12-07 10:20:24 +0800

    更新

--- 调试技巧/调试技巧.c
+++ 调试技巧/调试技巧.c
@@ -44,15 +44,20 @@
 //		;
 //	}
 //}
-//
-//int main()
-//{
-//	char arr1[20] = { "*************" };
-//	char arr2[] = "hello zhuzhu";
-//	/*strcpy(arr1, arr2);*/
-//	my_strcpy(arr1, arr2);
-//	printf("%s", arr1);
-//	return 0;
-//}
 
-
+void my_strcpy(char* dest, char* sor)
+{
+	while (*dest++ = *sor++)
+	{
+		;
+	}
+}
+int main()
+{
+	char arr1[20] = { "*************" };
+	char arr2[] = "hello zhuzhu";
+	/*strcpy(arr1, arr2);*/
+	my_strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
