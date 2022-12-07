5f67ee89413bd8fdfbb3757dc6c8c682608d516c
nangonglinger <1307998991@qq.com>
2022-12-07 09:44:26 +0800

    更新测试文件

---
+++ 调试技巧/调试技巧.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+
+
+//Çó1£¡+2£¡+3£¡+   +n£¡
+//int fact(int a)
+//{
+//	int i = 1;
+//	int ret = 1;
+//	for (i = 1; i <= a; i++)
+//	{
+//		ret = ret * i;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int i = 0;
+//	int tmp = 0;
+//	if (a == 0)
+//	{
+//		printf("0");
+//	}
+//	else
+//	{
+//		for (i = 1; i <= a; i++)
+//		{
+//			tmp = tmp + fact(i);
+//		}
+//		printf("%d", tmp);
+//	}
+//	return 0;
+//}
+
+void my_strcpy(char* dest, char* sor)
+{
+	while (*dest++ = *sor++)
+	{
+		;
+	}
+}
+
+int main()
+{
+	char arr1[20] = { "*************" };
+	char arr2[] = "hello zhuzhu";
+	/*strcpy(arr1, arr2);*/
+	my_strcpy(arr1, arr2);
+	printf("%s", arr1);
+	return 0;
+}
