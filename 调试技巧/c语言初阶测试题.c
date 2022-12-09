db7724141f1bb75536a7264c19c2d537f64c34b6
nangonglinger <1307998991@qq.com>
2022-12-09 10:43:51 +0800

    测试编程题

--- 调试技巧/c语言初阶测试题.c
+++ 调试技巧/c语言初阶测试题.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
+#include <string.h>
 
 //设计一个算法求两个正整数的最小公倍数
 //int LCM(int a, int b)
@@ -26,25 +27,39 @@
 //	return 0;
 //}
 
-void reverse_word(char* arr1)
-{
-	while (*arr1 != '\0')
-	{
-		if (*arr1 != ' ')
-		{
-			reverse_word(arr1 + 1);
-		}
-		printf("%c", *arr1);
-		arr1++;
-	}
-
-}
-
-int main()
-{
-	char arr1[100] = { 0 };
-	char arr2[100] = { 0 };
-	scanf("%s", arr1);
-	reverse_word(arr1);
-	return 0;
-}
+//void reverse_word(char* left, char* right)
+//{
+//	while (left < right)
+//	{
+//		int tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//
+//int main()
+//{
+//	char arr[100] = { 0 };
+//	gets(arr);
+//	int len = strlen(arr);
+//	reverse_word(arr,arr+len-1);
+//	char* start = arr;
+//
+//	while (*start)
+//	{
+//		char* end = start;
+//		while(*end != ' '&& *end != '\0')
+//		{
+//			end++;
+//		}
+//		reverse_word(start, end - 1);
+//		if (*end = ' ')
+//			start = end + 1;
+//		else
+//			start = end;
+//	}
+//	printf("%s", arr);
+//	return 0;
+//}
