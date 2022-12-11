8565451e323cb585674a8b5542768cdefbf5b966
nangonglinger <1307998991@qq.com>
2022-12-11 11:16:06 +0800

    1

--- 刷题/test.c
+++ 刷题/test.c
@@ -32,54 +32,62 @@
 //    printf("%lld %lld %lld", a / 3600, a / 60, a % 60);
 //    return 0;
 //}
+//
+//
+//void print_max(float arr[], int n)
+//{
+//    float ret = arr[0];
+//    for (int i = 0; i < n; i++)
+//    {
+//        if (ret < arr[i])
+//        {
+//             ret = arr[i];
+//        }
+//    }
+//    printf("%.2f ", ret);
+//}
+//void print_min(char arr[], int n)
+//{
+//    float ret = arr[0];
+//    for (int i = 0; i < n; i++)
+//    {
+//        if (ret > arr[i])
+//        {
+//            ret = arr[i];
+//        }
+//    }
+//    printf("%.2f ", ret);
+//}
+//print_average(char arr[], int n)
+//{
+//    float ret = 0;
+//    for (int i = 0; i < n; i++)
+//    {
+//        ret += arr[i];
+//    }
+//    printf("%.2f", ret);
+//}
+//int main() {
+//    int n = 0;
+//    scanf("%d", &n);
+//    int ch = getchar();
+//    float arr[100] = { 0 };
+//    int i = 0;
+//    for ( i = 0; i < n; i++)
+//    {
+//        scanf("%f", &arr[i]);
+//        int ch = getchar();
+//    }
+//    print_max(arr, n);
+//    print_min(arr, n);
+//    print_average(arr, n);
+//    return 0;
+//}
 
-
-void print_max(float arr[], int n)
-{
-    float ret = arr[0];
-    for (int i = 0; i < n; i++)
-    {
-        if (ret < arr[i])
-        {
-             ret = arr[i];
-        }
-    }
-    printf("%.2f ", ret);
-}
-void print_min(char arr[], int n)
-{
-    float ret = arr[0];
-    for (int i = 0; i < n; i++)
-    {
-        if (ret > arr[i])
-        {
-            ret = arr[i];
-        }
-    }
-    printf("%.2f ", ret);
-}
-print_average(char arr[], int n)
-{
-    float ret = 0;
-    for (int i = 0; i < n; i++)
-    {
-        ret += arr[i];
-    }
-    printf("%.2f", ret);
-}
-int main() {
-    int n = 0;
-    scanf("%d", &n);
-    int ch = getchar();
-    float arr[100] = { 0 };
-    int i = 0;
-    for ( i = 0; i < n; i++)
-    {
-        scanf("%f", &arr[i]);
-        int ch = getchar();
-    }
-    print_max(arr, n);
-    print_min(arr, n);
-    print_average(arr, n);
-    return 0;
-}
+//
+//    int main() {
+//    double f;
+//    scanf("%lf", &f);
+//    printf("%.3f", 5.0/9.0 * (f - 32));
+//    return 0;
+//}
