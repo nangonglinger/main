9935c94500585df71ed07ab633a1b0a1a243b815
nangonglinger <1307998991@qq.com>
2022-12-27 16:17:26 +0800

    1

--- 数据的存储/数据的存储.c
+++ 数据的存储/数据的存储.c
@@ -7,3 +7,28 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i++)
+//	{
+//		printf("%u\n", i);
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d", strlen(a));
+//	return 0;
+//}
+
+
