3ea01e80f99ded1447193296bc2a6f2bde24558a
nangonglinger <1307998991@qq.com>
2023-01-23 22:14:20 +0800

    1

--- 数据的存储/数据的存储.c
+++ 数据的存储/数据的存储.c
@@ -46,8 +46,13 @@
 //	return 0;
 //}
  
-int main()
-{
+//int main()
+//{
+//	int num1 = 1;
+//	int num2 = 3;
+//	scanf("%d %d", &num1, &num2);
+//	printf("1=%d 2=%d", num1, num2);
+//	return 0;
+//}
 
-	return 0;
-}
+ 
