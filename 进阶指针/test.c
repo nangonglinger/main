d5f393ad1ede1b2ce7fe3a0b3f4d22e7c0726247
nangonglinger <1307998991@qq.com>
2023-02-17 11:34:08 +0800

    1

---
+++ 进阶指针/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int(*pa)[10] = &arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *((*pa) + i));
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	int a = 6;
+	return 0;
+}
